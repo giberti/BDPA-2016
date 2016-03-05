d4c807da68e105ae7749dd23e45f761808fa81c8
Erik Giberti <erik@af-design.com>
2016-03-04 22:12:51 -0600

    Be less dumb about `Username` and return error messages to aid with debugging errors

--- MessageBoard/api/user.php
+++ MessageBoard/api/user.php
@@ -5,7 +5,8 @@ require __DIR__ . '/../setup.php';
 // Get our data from the request
 $method   = isset($_POST['method']) ? $_POST['method'] : null;
 $password = isset($_POST['password']) ? $_POST['password'] : null;
-$username = ctype_alnum($_POST['username']);
+$username = isset($_POST['username']) ? $_POST['username'] : null;
+$username = ctype_alnum($username) ? $username : null;
 
 // An associative array to hold information the frontend might need
 $responseData = array();
@@ -14,23 +15,27 @@ $responseData = array();
 switch ($method) {
     case 'addUser': // registers the provided username
 
-        if (addUser($username, $password)) {
+        if ($username && addUser($username, $password)) {
             // Send a 201 (created) to indicate the creation was successful
             http_response_code(201);
+            $responseData['message'] = 'created';
         } else {
             // send a 409 (conflict) to indicate there was a problem (usually a race condition on the username)
             http_response_code(409);
+            $responseData['message'] = 'conflict';
         }
         break;
 
     case 'isAvailable': // checks to see if the username is available
 
-        if (isAvailable($username)) {
+        if ($username && isAvailable($username)) {
             // send a 200 (ok) to indicate it's available
             http_response_code(200);
+            $responseData['message'] = 'ok';
         } else {
             // send a 409 (conflict) to indicate it's already in use
             http_response_code(409);
+            $responseData['message'] = 'conflict';
         }
         break;
 
@@ -39,15 +44,22 @@ switch ($method) {
         // Rotate the session
         session_regenerate_id(true);
 
-        $userId = checkLogin($username, $password);
+        if ($username) {
+            $userId = checkLogin($username, $password);
+        } else {
+            $responseData['message'] = 'who are you?';
+            break;
+        }
         if ($userId) {
             // send a 200 (ok) to indicate that the user is logged in
             http_response_code(200);
+            $responseData['message'] = 'ok';
             $_SESSION['Username'] = $username;
             $_SESSION['UserID'] = $userId;
         } else {
             // send a 401 (unauthorized) to indicate the login failed
             http_response_code(401);
+            $responseData['message'] = 'unauthorized';
             $_SESSION['Username'] = null;
             $_SESSION['UserID'] = null;
         }
@@ -56,6 +68,7 @@ switch ($method) {
     default:
         // Send a 400 (bad request) for unknown method
         http_response_code(400);
+        $responseData['message'] = 'wat?';
 }
 
 print json_encode($responseData);
