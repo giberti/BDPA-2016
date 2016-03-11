fc1f2ccf939371b1c47d5645661016adcb1d0733
Erik Giberti <erik@af-design.com>
2016-03-10 21:17:03 -0600

    Add the messages table and better indexing on user

--- MessageBoard/database.sql
+++ MessageBoard/database.sql
@@ -1,12 +1,22 @@
--- USERS Table
 CREATE TABLE IF NOT EXISTS `users` (
   `UserID` int(10) unsigned NOT NULL auto_increment,
-  `Username` varchar(50) character set utf8 NOT NULL,
-  `PasswordHash` varchar(50) character set utf8 NOT NULL,
-  `PasswordSalt` varchar(50) character set utf8 NOT NULL,
+  `Username` varchar(50) NOT NULL,
+  `PasswordHash` varchar(50)  NOT NULL,
+  `PasswordSalt` varchar(50) NOT NULL,
   `DateCreated` datetime default NULL,
   `LastLogin` datetime default NULL,
   PRIMARY KEY  (`UserID`),
-  UNIQUE KEY `Username` (`Username`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+  UNIQUE KEY `Username` (`Username`),
+  INDEX `LastLogin` (`LastLogin`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS `messages` (
+  `MessageID` int(10) unsigned NOT NULL auto_increment,
+  `ReplyTo` int(10) unsigned DEFAULT 0,
+  `UserID` int(10) unsigned NOT NULL,
+  `DatePosted` datetime NOT NULL,
+  `Message` text NOT NULL,
+  PRIMARY KEY  (`MessageID`),
+  INDEX `ReplyTo` (`ReplyTo`),
+  INDEX `DatePosted` (`DatePosted`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
