0f93aaf266cc46bade9b76999759a9b9a0b79be5
Erik Giberti <erik@af-design.com>
2016-03-12 09:14:53 -0600

    Make session unique per student

--- MessageBoard/session.php
+++ MessageBoard/session.php
@@ -1,3 +1,16 @@
 <?php
 
     session_start();
+    $sitehash = md5(__FILE__);
+    if (!isset($_SESSION['sitehash'])) {
+        $_SESSION['sitehash'] = $sitehash;
+    }
+
+    // Check and see if the current session is for this site, if not
+    // clear it.
+    if ($sitehash != $_SESSION['sitehash']) {
+        foreach (array_keys($_SESSION) as $key) {
+            unset($_SESSION[$key]);
+        }
+        $_SESSION['sitehash'] = $sitehash;
+    }
